d59fc1e482a1feef71ffb250ae20dba7e7169041
NIKATONI <nikoloz.iobishvili@gmail.com>
2025-08-29 13:07:28 +0400

    Registration Page

--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from './registrationPage.module.css'
 
 function RegistrationPage() {
   return (
-    <div>RegistrationPage</div>
+      <div class = {styles.login_box}>
+        <h2>Registration</h2>
+        <img src="/public/skillMap Icon.svg" alt="skillMap icon" className={styles.icon} />
+        <form>
+            <div class = {styles.user_box}>
+                <input type="text"/>
+                <label for="username">User</label>
+            </div>
+            <div class = {styles.user_box}>
+                <input type="password" id="password"/>
+                <label for="password">Password</label>
+            </div>
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+            </a>
+        </form>
+      </div>
   )
 }
 
