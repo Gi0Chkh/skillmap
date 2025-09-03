c7ed76faeb83a0557a43dc8cadf98f4969d281ef
NIKATONI <nikoloz.iobishvili@gmail.com>
2025-09-03 18:24:12 +0400

    Footer Header, login, reg styles

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,26 +1,49 @@
 import React, { useState, useContext, useRef } from "react";
-import styles from './loginPage.module.css'
-import Input from '../../components/Input/Input'
+import styles from "./loginPage.module.css";
+import Input from "../../components/Input/Input";
 import { LanguageContext } from "../../context/LanguageContext";
 
 function LoginPage() {
   const { lang } = useContext(LanguageContext);
   return (
-      <div class = {styles.login_box}>
-        <img src="/public/skillMap Icon.svg" alt="skillMap icon" className={styles.icon} />
-        <form>
-            <div class = {styles.user_box}>
-                <Input type="text" placeholder={lang === "EN" ? "Email" : "მეილი"}/>
-            </div>
-            <div class = {styles.user_box}>
-                <Input type="password" id="password" placeholder={lang === "EN" ? "Password" : "პაროლი"}/>
-            </div>
-            <a href='#' className={styles.forgotPassword}>{lang === "EN" ? "Forgot password" : "დაგავიწყდა პაროლი?"}</a>
-            <button onClick="#">{lang === "EN" ? "Log In" : "შესვლა"}</button>
-            <p>{lang === "EN" ? "Don't have an account?" : "არ გაქვს აქაუნთი?"} <a href='/register' className={styles.dontHaveAcc}>{lang === "EN" ? "Sign Up" : "შესვლა"}</a></p>
-        </form>
-      </div>
-  )
+    <div class={styles.login_box}>
+      <img
+        src="/public/skillMap(no text).svg"
+        alt="skillMap icon"
+        className={styles.icon}
+      />
+      <form>
+        <h2>SkillMap</h2>
+        <div className={styles.inputs}>
+          <div class={styles.user_box}>
+            <Input
+              type="text"
+              placeholder={lang === "EN" ? "Email" : "მეილი"}
+            />
+          </div>
+          <div class={styles.user_box}>
+            <Input
+              type="password"
+              id="password"
+              placeholder={lang === "EN" ? "Password" : "პაროლი"}
+            />
+          </div>
+          <a href="#" className={styles.forgotPassword}>
+            {lang === "EN" ? "Forgot password?" : "დაგავიწყდა პაროლი?"}
+          </a>
+        </div>
+        <button className={styles.loginButton} onClick="#">
+          {lang === "EN" ? "Log In" : "შესვლა"}
+        </button>
+        <p>
+          {lang === "EN" ? "Don't have an account?" : "არ გაქვს აქაუნთი?"}{" "}
+          <a href="/register" className={styles.dontHaveAcc}>
+            {lang === "EN" ? "Sign Up" : "შესვლა"}
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/pages/Registration/RegistrationPage.jsx
+++ src/pages/Registration/RegistrationPage.jsx
@@ -1,28 +1,46 @@
 import React, { useState, useContext, useRef } from "react";
-import styles from './registrationPage.module.css'
-import Input from '../../components/Input/Input'
+import styles from "./registrationPage.module.css";
+import Input from "../../components/Input/Input";
 import { LanguageContext } from "../../context/LanguageContext";
 
 function RegistrationPage() {
   const { lang } = useContext(LanguageContext);
   return (
-      <div class = {styles.registration_box}>
-        <img src="/public/skillMap Icon.svg" alt="skillMap icon" className={styles.icon} />
-        <form>
-            <div class = {styles.user_box}>
-                <Input type="text" placeholder={lang === "EN" ? "Email" : "შესვლა"}/>
-            </div>
-            <div class = {styles.user_box}>
-                <Input type="password" id="password" placeholder={lang === "EN" ? "Password" : "პაროლი"}/>
-            </div>
-            <button onClick="#">{lang === "EN" ? "Sign up" : "დარეგისტრირდი"}</button>
-            <p>
-              {lang === "EN" ? "Already have an account?" : "უკვე გაქვს აქაუნთი?"} <a href='/login' className={styles.dontHaveAcc}>
-              {lang === "EN" ? "Sign In" : "შესვლა"}</a>
-            </p>
-        </form>
-      </div>
-  )
+    <div class={styles.registration_box}>
+      <img
+        src="/public/skillMap(no text).svg"
+        alt="skillMap icon"
+        className={styles.icon}
+      />
+      <form>
+        <h2>SkillMap</h2>
+        <div className={styles.inputs}>
+          <div class={styles.user_box}>
+            <Input
+              type="text"
+              placeholder={lang === "EN" ? "Email" : "შესვლა"}
+            />
+          </div>
+          <div class={styles.user_box}>
+            <Input
+              type="password"
+              id="password"
+              placeholder={lang === "EN" ? "Password" : "პაროლი"}
+            />
+          </div>
+        </div>
+        <button className={styles.registrationButton} onClick="#">
+          {lang === "EN" ? "Sign up" : "დარეგისტრირდი"}
+        </button>
+        <p>
+          {lang === "EN" ? "Already have an account?" : "უკვე გაქვს აქაუნთი?"}{" "}
+          <a href="/login" className={styles.dontHaveAcc}>
+            {lang === "EN" ? "Log In" : "შესვლა"}
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default RegistrationPage
+export default RegistrationPage;
